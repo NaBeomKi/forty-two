76ba2fac295b4e163bacc151c0e2a2fdecbfef4c
나범기 <ki@nabeomgiui-MacBookAir.local>
2021-04-04 23:16:15 +0900

    still fix ex10 && ex11

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: bna <bna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 14:02:10 by bna               #+#    #+#             */
-/*   Updated: 2021/04/04 05:07:12 by bna              ###   ########.fr       */
+/*   Updated: 2021/04/04 21:44:00 by bna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	while (i < n)
 	{
 		dest[i] = 0;
+		i++;
 	}
 	return (dest);
 }
