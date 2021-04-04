14b22b32e1af42fe9b309deb7588ed3f9797dcb2
나범기 <ki@nabeomgiui-MacBookAir.local>
2021-04-05 01:40:25 +0900

    still ex04

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -6,23 +6,27 @@
 /*   By: bna <bna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 00:41:27 by bna               #+#    #+#             */
-/*   Updated: 2021/04/04 00:43:22 by bna              ###   ########.fr       */
+/*   Updated: 2021/04/05 00:53:40 by bna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int i;
+	unsigned int j;
 
 	i = 0;
 	while (dest[i])
 	{
 		i++;
 	}
-	while (i < nb)
+	j = 0;
+	while (j < nb)
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
 		i++;
+		j++;
 	}
+	dest[i] = 0;
 	return (dest);
 }
