2522b515687754e8cf94cfa819eec2a108be3171
나범기 <ki@nabeomgiui-MacBookAir.local>
2021-04-07 01:12:34 +0900

    not yet

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,11 +6,46 @@
 /*   By: bna <bna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 21:13:48 by bna               #+#    #+#             */
-/*   Updated: 2021/04/06 21:34:27 by bna              ###   ########.fr       */
+/*   Updated: 2021/04/07 01:11:43 by bna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putnbr_base(int nbr, char *base)
-{}
+int     len(char *base)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (base[i])
+    {
+        if (base[i] == '+' || base[i] == '-')
+            return (0);
+        if (base[i + 1])
+        {
+            j = 0;
+            while (base[i + 1 + j])
+            {
+                if (base[i] == base[i + 1 + j])
+                    return (0);
+                j++;
+            }
+        }
+        i++;
+    }
+    if (!*base || i == 1)
+        return (0);
+    return (i);
+}
+
+void    ft_putnbr_base(int nbr, char *base)
+{
+    int base_num;
+
+    base_num = len(base);
+    if (!base_num)
+        return;
+    
+    
+}
