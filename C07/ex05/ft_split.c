4022cb608b0d5dcdd21add8d18ac3ca18ba966fd
나범기 <ki@nabeomgiui-MacBookAir.local>
2021-04-13 20:58:07 +0900

    add split

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -6,50 +6,89 @@
 /*   By: bna <bna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 22:27:57 by bna               #+#    #+#             */
-/*   Updated: 2021/04/13 01:24:13 by bna              ###   ########.fr       */
+/*   Updated: 2021/04/13 20:19:03 by bna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strstr(char *str, char *to_find)
-{
-	int		i;
-	int		j;
+char	*g_str;
+int		g_total_len;
+int		g_count;
 
-	if (!*to_find)
-		return (str);
+int		ft_strlen(char *str)
+{
+	int i;
+
 	i = 0;
+	while (*str++)
+		i++;
+	return (i);
+}
+
+void	ft_strcpy(char *str, char *charset)
+{
+	int i;
+	int j;
+	int flag;
+
+	i = 0;
+	flag = 0;
 	while (str[i])
 	{
-		if (str[i] == to_find[0])
+		j = 0;
+		while (charset[j])
 		{
-			j = 0;
-			while (to_find[j])
+			if (str[i] == charset[j])
 			{
-				if (str[i + j] != to_find[j])
-				{
-					break ;
-				}
-				j++;
+				g_str[i] = 0;
+				flag = 0;
+				break ;
 			}
-			if (!to_find[j])
-				return (&str[i]);
+			j++;
+		}
+		if (!charset[j])
+		{
+			!flag ? g_count++ : g_count;
+			g_str[i] = str[i];
+			flag = 1;
 		}
 		i++;
 	}
-	return (0);
+	g_str[i] = 0;
 }
 
-char    **ft_split(char *str, char *charset)
+char	**ft_split(char *str, char *charset)
 {
-    char *tmp;
-    int i;
+	char	**result;
+	int		i;
+	int		j;
+	int		flag;
 
-    tmp = str;
-    while (*tmp)
-    {
-        
-        i++;
-    }
+	g_count = 0;
+	g_total_len = ft_strlen(str);
+	g_str = (char *)malloc(sizeof(char) * (g_total_len + 1));
+	ft_strcpy(str, charset);
+	result = (char **)malloc(sizeof(char *) * g_count + 1);
+	i = 0;
+	j = 0;
+	flag = 0;
+	while (i < g_count)
+	{
+		while (j < g_total_len)
+		{
+			if (!flag && g_str[j])
+			{
+				result[i] = &g_str[j];
+				flag = 1;
+				break ;
+			}
+			if (!g_str[j])
+				flag = 0;
+			j++;
+		}
+		i++;
+	}
+	result[i] = 0;
+	return (result);
 }
--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,15 +6,16 @@
 /*   By: bna <bna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 02:40:23 by bna               #+#    #+#             */
-/*   Updated: 2021/04/13 03:17:07 by bna              ###   ########.fr       */
+/*   Updated: 2021/04/13 18:40:49 by bna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 typedef struct s_stock_str
 {
-int size;
-char *str;
-char *copy;
+    int size;
+    char *str;
+    char *copy;
 } t_stock_str;
 
 struct s_stock_str *ft_strs_to_tab(int ac, char **av)
