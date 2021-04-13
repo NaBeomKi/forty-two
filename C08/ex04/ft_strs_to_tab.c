f8d66f4fddba38ffdd34f8639ebb085dce08263f
나범기 <ki@nabeomgiui-MacBookAir.local>
2021-04-14 01:21:37 +0900

    add pdf & test case

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,11 +6,13 @@
 /*   By: bna <bna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 02:40:23 by bna               #+#    #+#             */
-/*   Updated: 2021/04/13 18:40:49 by bna              ###   ########.fr       */
+/*   Updated: 2021/04/14 01:20:55 by bna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_stock_str.h"
 #include <stdlib.h>
+
 typedef struct s_stock_str
 {
     int size;
@@ -20,5 +22,9 @@ typedef struct s_stock_str
 
 struct s_stock_str *ft_strs_to_tab(int ac, char **av)
 {
-    
+    t_stock_str *stock;
+    int         i;
+
+    stock = malloc(sizeof(t_stock_str) * (ac + 1));
+
 }
