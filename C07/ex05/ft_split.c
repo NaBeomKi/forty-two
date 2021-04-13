664eece2f3b597b1de97d52c09e1ae0f4b7a966e
나범기 <ki@nabeomgiui-MacBookAir.local>
2021-04-13 23:08:03 +0900

    finished c07 ex05

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -6,15 +6,19 @@
 /*   By: bna <bna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 22:27:57 by bna               #+#    #+#             */
-/*   Updated: 2021/04/13 20:19:03 by bna              ###   ########.fr       */
+/*   Updated: 2021/04/13 23:06:55 by bna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 char	*g_str;
+char	**g_result;
 int		g_total_len;
 int		g_count;
+int		g_i;
+int		g_j;
+int		g_flag;
 
 int		ft_strlen(char *str)
 {
@@ -28,67 +32,62 @@ int		ft_strlen(char *str)
 
 void	ft_strcpy(char *str, char *charset)
 {
-	int i;
-	int j;
-	int flag;
-
-	i = 0;
-	flag = 0;
-	while (str[i])
+	g_i = 0;
+	g_flag = 0;
+	while (str[g_i])
 	{
-		j = 0;
-		while (charset[j])
+		g_j = 0;
+		while (charset[g_j])
 		{
-			if (str[i] == charset[j])
+			if (str[g_i] == charset[g_j])
 			{
-				g_str[i] = 0;
-				flag = 0;
+				g_str[g_i] = 0;
+				g_flag = 0;
 				break ;
 			}
-			j++;
+			g_j++;
 		}
-		if (!charset[j])
+		if (!charset[g_j])
 		{
-			!flag ? g_count++ : g_count;
-			g_str[i] = str[i];
-			flag = 1;
+			!g_flag ? g_count++ : g_count;
+			g_str[g_i] = str[g_i];
+			g_flag = 1;
 		}
-		i++;
+		g_i++;
 	}
-	g_str[i] = 0;
+	g_str[g_i] = 0;
+}
+
+void	get_result(void)
+{
+	g_i = 0;
+	g_j = 0;
+	g_flag = 0;
+	while (g_i < g_count)
+	{
+		while (g_j < g_total_len)
+		{
+			if (!g_flag && g_str[g_j])
+			{
+				g_result[g_i] = &g_str[g_j];
+				g_flag = 1;
+				break ;
+			}
+			g_flag = !g_str[g_j] ? 0 : g_flag;
+			g_j++;
+		}
+		g_i++;
+	}
+	g_result[g_i] = 0;
 }
 
 char	**ft_split(char *str, char *charset)
 {
-	char	**result;
-	int		i;
-	int		j;
-	int		flag;
-
 	g_count = 0;
 	g_total_len = ft_strlen(str);
 	g_str = (char *)malloc(sizeof(char) * (g_total_len + 1));
 	ft_strcpy(str, charset);
-	result = (char **)malloc(sizeof(char *) * g_count + 1);
-	i = 0;
-	j = 0;
-	flag = 0;
-	while (i < g_count)
-	{
-		while (j < g_total_len)
-		{
-			if (!flag && g_str[j])
-			{
-				result[i] = &g_str[j];
-				flag = 1;
-				break ;
-			}
-			if (!g_str[j])
-				flag = 0;
-			j++;
-		}
-		i++;
-	}
-	result[i] = 0;
-	return (result);
+	g_result = (char **)malloc(sizeof(char *) * g_count + 1);
+	get_result();
+	return (g_result);
 }
